e9c15b46243647d21a676a0e6d3234854ecbc647
chingchunlin890531 <chingchunlin890531@gmail.com>
2022-01-18 18:27:43 +0800

    0118(2)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '/Users/veronica.lin/reactJS/hello-test/src/App.css';
 function About(){
     return(
         <div>
             <h1>About</h1>
-            <nav>
-                <Link className="App-link" to="/">Home</Link>
-                <Link className="App-link" to="/About" style={{marginLeft:"20px"}}>About</Link>
-                <Link className="App-link" to="/Profile" style={{marginLeft:"20px"}}>Profile</Link>
-            </nav> 
+            <div className="navmenu">
+            <div className="leftb">
+                <Link className="item" to="/">Home</Link>
+                <Link className="item" to="/About">About</Link>
+                <Link className="item" to="/Profile">Profile</Link>
+            </div>
+            </div>
         </div>
     );
 };
